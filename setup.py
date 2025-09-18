646a63a2c39042bdb0004e0463cd86ac0b659a29
FAKER-112 <abdulazeezadegbiji@gmail.com>
2025-09-18 20:24:07 +0100

    Modified filename with latest updates

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT= '-e.'
+def get_requirement(file_path:str):
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [requirement.replace('/n','') for requirement in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+name='project_0001',
+version='error_404',
+author='Seaforth Zanthus',
+author_email='error_404@gmail.com',# authorrrr doesnt have email :(
+packages=find_packages(),
+install_requires=get_requirement('requirements.txt')
+
+)
